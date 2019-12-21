72ddd7db5b68ec8a06a936825de063f028722fbb
TrojanNikita <nikitatrojan@mail.ru>
2019-12-21 09:25:23 +0300

    Все из списка

--- src/actions/actionTodo.ts
+++ src/actions/actionTodo.ts
@@ -1,55 +1,13 @@
-import {ADD_TODO,DELETE_COMPLETED_TODOS,
-        DELETE_TODO,EDIT_TODO,
-        TOGGLE_ALL,TOGGLE_TODO, 
-        SET_PRIORITY,TODOS_LOADED,ERROR,LOAD} from './../constants/actions';
+import {addTodo,toggleTodo,
+        handleError,todosLoaded,
+        editTodo,deleteCompleted,deleteTodo,
+        setPriority, requestToServer, toggleAll
+} from './actions'
 import {Todo} from './../types/Todo'
 
-import { ActionTypeTodo, GetActionThunk,PostActionThunk} from '../types';
+import {  GetActionThunk,PostActionThunk} from '../types';
 
 import {fetchData} from '../services/todo-service';
-
-
-//Actions
-export const handleError = ():ActionTypeTodo => ({    
-        type: ERROR
-});
-export const requestToServer = ():ActionTypeTodo => ({    
-        type: LOAD
-});
-export const todosLoaded = (todos:Todo[]):ActionTypeTodo => ({    
-        type: TODOS_LOADED,
-        todos    
-});
-export const addTodo=(todo:Todo):ActionTypeTodo=>({
-        type: ADD_TODO,
-        todo
-})
-export const toggleTodo=(id:number):ActionTypeTodo=>({
-        type: TOGGLE_TODO,
-        id
-})
-export const editTodo=(id:number,label:string):ActionTypeTodo=>({
-        type: EDIT_TODO,
-        id,
-        label
-})
-export const deleteTodo=(id:number):ActionTypeTodo=>({
-        type: DELETE_TODO,
-        id
-})
-export const setPriority=(id:number,priority:number):ActionTypeTodo=>({
-        type: SET_PRIORITY,
-        id,
-        priority
-})
-export const toggleAll=(flag:boolean):ActionTypeTodo=>({
-        type: TOGGLE_ALL,
-        flag
-})
-export const deleteCompleted=():ActionTypeTodo=>({
-        type: DELETE_COMPLETED_TODOS
-})
-
 
 //ThunkActions
 export const addTodoThunk= (label:string):PostActionThunk=>async dispatch => {
